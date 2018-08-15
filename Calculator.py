d1246884ebbfe7a2699f798e9e0e5e5a1708f241
SamCartledge <cartledgesam15@gmail.com>
2018-08-14 19:32:26 -0700

    Adding New Calculator program and updated ThankYou

---
+++ Calculator.py
@@ -0,0 +1,44 @@
+import time
+
+myName="Sam"
+myAge="16"
+yourName=input("What is your name?")
+yourAge=int(input("What is your age?"))
+
+print("Hello",yourName)
+
+
+#time.sleep(1)  
+
+if yourAge>30:
+    print("You are old")
+else: 
+    print("You are young")
+
+#time.sleep(1)  
+
+print("My name is ",myName)
+
+#time.sleep(1)  
+
+print("And I am ",myAge, "years old.")
+
+#ask manager how much for 8gb ram
+#ASK CUSTOMER how many sticks of 8gb ram
+#print out customer's total order
+#then calculate tax(@8%)
+#print tax and grand total
+bossPrice=float(input("What is the price of an 8gb stick of ram?"))
+
+customerorder=float(input("How many 8gb sticks would you like?"))
+
+print("Ok, so you would like",customerorder, "sticks of 8gb ram priced at",bossPrice)
+
+totalPrice=(customerorder*(bossPrice))
+
+tax=(totalPrice*(0.08))
+
+grandTotal=totalPrice+tax
+
+print("Your tax comes to $",round(tax,2))
+print("Your total comes to $",round(grandTotal,2),"Thank you for your purchase.")
--- ThankYou.py
+++ ThankYou.py
@@ -1,3 +1,5 @@
+import time
+
 myName="Sam"
 myAge="16"
 yourName=input("What is your name?")
@@ -5,10 +7,24 @@ yourAge=int(input("What is your age?"))
 
 print("Hello",yourName)
 
+
+time.sleep(3)  
+
 if yourAge>30:
     print("You are old")
 else: 
     print("You are young")
 
+time.sleep(3)  
+
 print("My name is ",myName)
+
+time.sleep(3)  
+
 print("And I am ",myAge, "years old.")
+
+yourPrice=float(input("What is your price?"))
+
+yourPrice=yourPrice+(yourPrice*0.08)
+
+print("$",yourPrice)
