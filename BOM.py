637412eb15f0ad25817e35c3284fe0ebaeae7d9b
SamCartledge <cartledgesam15@gmail.com>
2018-08-28 19:36:53 -0700

    i did stuff

--- BOM.py
+++ BOM.py
@@ -1,46 +1,24 @@
-import time
+import json
 
-myName="Sam"
-myAge="16"
-yourName=input("What is your name?")
-yourAge=int(input("What is your age?"))
+jsonFIle = open("./machines.json")
 
-print("Hello",yourName)
+machines = json.load(jsonFIle)
 
-#time.sleep(1)  
+print("Let's Build a PC that fits your budget. What setup type would you like? We offer the Budget Gamer, the Mid Ranger, the Power Budget, and the All Out Power.")
 
-if yourAge>30:
-    print("You are old")
-else: 
-    print("You are young")
+for machineCounter in range(len(machines)):
+    machines[machineCounter]["systemName"]
+    print(machines[machineCounter]["systemName"])
 
-#time.sleep(1)  
 
-print("My name is ",myName)
 
-#time.sleep(1)  
 
-print("And I am ",myAge, "years old.")
+#print("Ok, so you would like the", machines, "priced at $",price)
 
-#ask manager how much for 8gb ram
-#ASK CUSTOMER how many sticks of 8gb ram
-#print out customer's total order
-#then calculate tax(@8%)
-#print tax and grand total
 
-print("Let's Build a PC that fits your budget. What setup type would you like? We offer the Budget Gamer, ")
-bossPrice=float(input("What is the price of an 8gb stick of ram?"))
+#tax=(price*(0.08))
 
-customerorder=float(input("How many 8gb sticks would you like?"))
+#grandTotal=totalPrice+tax
 
-print("Ok, so you would like",customerorder, "sticks of 8gb ram priced at",bossPrice)
-
-totalPrice=(customerorder*(bossPrice))
-
-tax=(totalPrice*(0.08))
-
-grandTotal=totalPrice+tax
-
-print("Your tax comes to $",round(tax,2))
-print("Your total comes to $",round(grandTotal,2),"Thank you for your purchase.")
-hereit
+#print("Your tax comes to $",round(tax,2))
+#print("Your total comes to $",round(grandTotal,2),"Thank you for your purchase.")
